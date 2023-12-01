04aedd4dda17d9e0fa79bbd161920feb3122b1e4
Dan Kokron <Daniel.Kokron@noaa.gov>
2023-12-01 15:48:45 +0000

    Add compile time option to use MKL Merseene twister

--- physics/GFS_rad_time_vary.fv3.F90
+++ physics/GFS_rad_time_vary.fv3.F90
@@ -1,4 +1,7 @@
 !>\file GFS_rad_time_vary.fv3.F90
+#ifdef USE_MKL_MERSENNE
+include 'mkl_vsl.f90'
+#endif
 !!  Contains code related to GFS radiation suite setup (radiation part of time_vary_step)
    module GFS_rad_time_vary
 
@@ -25,7 +28,10 @@
          use mersenne_twister,          only: random_setseed, random_index, random_stat
          use machine,                   only: kind_phys
          use radcons,                   only: qmin, con_100
-
+#ifdef USE_MKL_MERSENNE
+         use mkl_vsl_type
+         use mkl_vsl
+#endif
          implicit none
 
          ! Interface variables
@@ -51,7 +57,12 @@
          type (random_stat) :: stat
          integer :: ix, j, i, ipseed
          integer :: numrdm(cnx*cny*2)
-
+#ifdef USE_MKL_MERSENNE
+         real(kind=4) :: subDomain(size(jmap))
+         type (VSL_STREAM_STATE) :: stream
+         integer(kind=8) SkipCount
+         integer(kind=4) err
+#endif
          ! Initialize CCPP error handling variables
          errmsg = ''
          errflg = 0
@@ -70,6 +81,35 @@
                enddo
              else
                ipseed = mod(nint(con_100*sqrt(sec)), ipsdlim) + 1 + ipsd0
+#ifdef USE_MKL_MERSENNE
+               ! Create a new stream using the new seed
+               err=vslnewstream( stream, VSL_BRNG_MT19937,  ipseed )
+
+               ! Skip ahead to local ranks section of the output.
+               ! Avoids having every rank compute random numbers for the whole grid.
+               SkipCount = int((imap(1)+isc-1+(jmap(1)+jsc-2)*cnx)-1,8)
+               err=vslskipaheadstream( stream,  SkipCount)
+
+               ! Compute local ranks section of the output
+               err=vsrnguniform( VSL_RNG_METHOD_UNIFORM_STD, stream, size(icsdsw), subDomain, real(0.0,4), real(ipsdlim,4) )
+               icsdsw=int(subDomain)
+
+               !do ix=1,size(jmap)
+               !  j = jmap(ix)
+               !  i = imap(ix)
+               !  write(6,'("RNG: ",i12)') (i+isc-1 + (j+jsc-2)*cnx)
+               !enddo
+
+               ! Skip ahead again
+               err=vslskipaheadstream( stream,  int(cnx*cny-size(icsdsw),8))
+               err=vsrnguniform( VSL_RNG_METHOD_UNIFORM_STD, stream, size(icsdlw), subDomain, real(0.0,4), real(ipsdlim,4) )
+               icsdlw=int(subDomain)
+               !write(6,'("RNG: ",4i12)') ipseed, SkipCount, cnx*cny, size(icsdlw)
+               !write(6,'("RNG: ",9i12)') ipseed, size(jmap), isc, jsc, imap(1), jmap(1), imap(size(jmap)), jmap(size(jmap)), cnx*cny
+
+               ! Delete the stream
+               err=vsldeletestream( stream )
+#else
                call random_setseed (ipseed, stat)
                call random_index (ipsdlim, numrdm, stat)
 
@@ -80,6 +120,7 @@
                  icsdsw(ix) = numrdm(i+isc-1 + (j+jsc-2)*cnx)
                  icsdlw(ix) = numrdm(i+isc-1 + (j+jsc-2)*cnx + cnx*cny)
                enddo
+#endif
              end if !lrseeds
            endif  ! isubc_lw and isubc_sw
 
